785be6c2b73aa62e79b93c78af29b2b7e8d7813a
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2026-05-11 19:04:24 +0200

    world map: fix route line z-fighting by staggering outline/fill Y and disabling depthWrite

--- src/render/MapRoute.tsx
+++ src/render/MapRoute.tsx
@@ -8,20 +8,25 @@ import { useGame } from "../store";
 export const MapRoute = () => {
   const progress = useGame((s) => s.progress);
 
+  type Seg = { outline: [THREE.Vector3, THREE.Vector3]; fill: [THREE.Vector3, THREE.Vector3] };
   const { reachedSegments, lockedSegments } = useMemo(() => {
-    const reached: [THREE.Vector3, THREE.Vector3][] = [];
-    const locked: [THREE.Vector3, THREE.Vector3][] = [];
+    const reached: Seg[] = [];
+    const locked: Seg[] = [];
 
     for (let i = 0; i < LEVELS.length - 1; i++) {
       const a = LEVELS[i];
       const b = LEVELS[i + 1];
       const bothUnlocked = isLevelUnlocked(a.id, progress) && isLevelUnlocked(b.id, progress);
-      const from = new THREE.Vector3(a.nodePos.x, 0.02, -a.nodePos.y);
-      const to = new THREE.Vector3(b.nodePos.x, 0.02, -b.nodePos.y);
+      const yOutline = 0.02;
+      const yFill = 0.025;
+      const fromOutline = new THREE.Vector3(a.nodePos.x, yOutline, -a.nodePos.y);
+      const toOutline = new THREE.Vector3(b.nodePos.x, yOutline, -b.nodePos.y);
+      const fromFill = new THREE.Vector3(a.nodePos.x, yFill, -a.nodePos.y);
+      const toFill = new THREE.Vector3(b.nodePos.x, yFill, -b.nodePos.y);
       if (bothUnlocked) {
-        reached.push([from, to]);
+        reached.push({ outline: [fromOutline, toOutline], fill: [fromFill, toFill] });
       } else {
-        locked.push([from, to]);
+        locked.push({ outline: [fromOutline, toOutline], fill: [fromFill, toFill] });
       }
     }
 
@@ -30,16 +35,44 @@ export const MapRoute = () => {
 
   return (
     <group>
-      {reachedSegments.map(([from, to], i) => (
+      {reachedSegments.map((seg, i) => (
         <group key={`r-${i}`}>
-          <Line points={[from, to]} color="#0a0a12" lineWidth={8} transparent opacity={0.7} />
-          <Line points={[from, to]} color="#ffd66a" lineWidth={5} transparent opacity={1} />
+          <Line
+            points={seg.outline}
+            color="#0a0a12"
+            lineWidth={8}
+            transparent
+            opacity={0.7}
+            depthWrite={false}
+          />
+          <Line
+            points={seg.fill}
+            color="#ffd66a"
+            lineWidth={5}
+            transparent
+            opacity={1}
+            depthWrite={false}
+          />
         </group>
       ))}
-      {lockedSegments.map(([from, to], i) => (
+      {lockedSegments.map((seg, i) => (
         <group key={`l-${i}`}>
-          <Line points={[from, to]} color="#0a0a12" lineWidth={7} transparent opacity={0.6} />
-          <Line points={[from, to]} color="#556070" lineWidth={4} transparent opacity={0.75} />
+          <Line
+            points={seg.outline}
+            color="#0a0a12"
+            lineWidth={7}
+            transparent
+            opacity={0.6}
+            depthWrite={false}
+          />
+          <Line
+            points={seg.fill}
+            color="#556070"
+            lineWidth={4}
+            transparent
+            opacity={0.75}
+            depthWrite={false}
+          />
         </group>
       ))}
     </group>
